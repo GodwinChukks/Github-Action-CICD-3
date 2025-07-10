b71e0357c00f64823f6511fde262fa3b5f1d33ee
Godwin <godwinchukks@gmail.com>
2025-07-10 18:21:30 +0000

    Error fixed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/testnode', (_req, res) => {
-    res.status(200).send('Yes the testNode endpoint is working');
+    res.status(200).send('Yes the testnode endpoint is working');
 });
 
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,12 @@
 const app = require('./app');
 const supertest = require('supertest');
-const {describe, it } = require('jest-circus');
+// const {describe, it } = require('jest-circus');
 const request = supertest(app);
 
-describe('/testNode endpoint', () => {
+describe('/testnode endpoint', () => {
   it('should return a response', async () => {
     const response = await request.get('/testNode');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Yes the testNode endpoint is working');
+    expect(response.text).toBe('Yes the testnode endpoint is working');
  });
 });
